9a1856854f0b8c0828f0079c22442ff0c4f553e6
lwmfjc <lwmfjc@gmail.com>
2022-07-03 23:57:13 +0800

    pom测试

---
+++ Hello/src/main/java/com/bjpowernode/HelloMaven.java
@@ -0,0 +1,13 @@
+package com.bjpowernode;
+
+public class HelloMaven{
+ public int addNumber(int n1,int n2){
+ 	System.out.println("hello maven -addNumber");
+ 	return n1+n2;
+  }
+  public static void main(String args[]){
+  	HelloMaven hello=new HelloMaven();
+  	int res=hello.addNumber(10,20);
+  	System.out.println("在main方法中，执行hello的方法="+res);
+  }
+}
